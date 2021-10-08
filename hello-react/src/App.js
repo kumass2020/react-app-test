2955d74c07b9dd21be3a5134d8aac9849b5493ea
hoho <kumass2021@gmail.com>
2021-10-09 03:04:53 +0900

    props and state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import MyComponent from './MyComponent';
+// import MyComponent from './MyComponent';
+// import Counter from './Counter';
+import Say from './Say';
+
+// const App = () => {
+//   return <MyComponent name="React" favoriteNumber={1}>
+//       리액트
+//     </MyComponent>;
+// };
+
+// const App = () => {
+//   return <Counter />;
+// };
 
 const App = () => {
-  return <MyComponent name="React" favoriteNumber={1}>
-      리액트
-    </MyComponent>;
+  return <Say />;
 };
 
 export default App;
