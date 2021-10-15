0556d85b613deb46df28fd08030a7945aeb02900
hoho <kumass2021@gmail.com>
2021-10-16 05:35:49 +0900

    ~Chapter 07

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ErrorBoundary from './ErrorBoundary';
 
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
@@ -34,19 +35,56 @@ import React, { Component } from 'react';
 //   }
 // }
 
-import ScrollBox from './ScrollBox';
+// import ScrollBox from './ScrollBox';
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+//         <button onClick={() => this.scrollBox.ScrollToBottom()}>
+//           맨 밑으로
+//         </button>
+//       </div>
+//     )
+//   }
+// }
+
+// import IterationSample from './IterationSample';
+// class App extends Component {
+//   render() {
+//     return (
+//       <IterationSample/>
+//     );
+//   }
+// }
+
+import LifeCycleSample from './LifeCycleSample';
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+        color: getRandomColor()
+    });
+  }
+
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-        <button onClick={() => this.scrollBox.ScrollToBottom()}>
-          맨 밑으로
-        </button>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
       </div>
     )
   }
 }
+
 
 export default App;
