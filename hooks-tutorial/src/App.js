f5bedaf2068bf392f741150f7d01a0273094782c
hoho <kumass2021@gmail.com>
2021-10-23 04:20:30 +0900

    ~ch08

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+// import Counter from './Counter';
+// const App = () => {
+//   return <Counter/>;
+// };
+
+// import Info from './Info';
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setVisible(!visible);
+//         }}
+//       >
+//         {visible ? '숨기기' : '보이기'}
+//       </button>
+//       <hr />
+//       {visible && <Info />}
+//     </div>
+//   );
+// };
+
+// import Info from './Info';
+// const App = () => {
+//   return <Info />;
+// };
+
+import Average from './Average';
+const App = () => {
+  return <Average />;
+}
+export default App;
