8f9d4211ddb4c36bf068633fd1a2e3c72a99998d
hoho <kumass2021@gmail.com>
2023-01-28 15:10:12 +0900

    ~ch24

---
+++ blog/blog-frontend/src/components/base/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const HeaderBlock = styled.div``;
+
+const Header = () => {
+    return (
+        <>
+            <HeaderBlock>
+
+            </HeaderBlock>
+        </>
+    );
+};
+
+export default Header;
---
+++ blog/blog-frontend/src/pages/PostPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostPage = () => {
+    return (
+        <div>
+            read post
+        </div>
+    );
+};
+
+export default PostPage;
