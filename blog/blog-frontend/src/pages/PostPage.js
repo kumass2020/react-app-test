a3e0234d631b761469216dcc962a1102acfe5c68
hoho <kumass2021@gmail.com>
2023-01-30 22:11:41 +0900

    ~Ch26 (Cont.)

--- blog/blog-frontend/src/pages/PostPage.js
+++ blog/blog-frontend/src/pages/PostPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import PostViewerContainer from '../containers/post/PostViewerContainer';
 
 const PostPage = () => {
     return (
-        <div>
-            read post
-        </div>
+        <>
+            <HeaderContainer />
+            <PostViewerContainer />
+        </>
     );
 };
 
